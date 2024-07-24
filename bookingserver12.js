0d70275f36e67d3f3ea6a23a26de5c97acf39565
lovis-ambrose <lovisambrose212@gmail.com>
2024-07-24 22:18:30 +0300

    small fix

--- bookingserver12.js
+++ bookingserver12.js
@@ -1,22 +1,33 @@
-const hostname = '127.0.0.1';
-const port = 8080;
-const usernamePassword = "bitebooker:bitebooker1";
-
-const https = require('https');
-const fs = require('fs');
+require('dotenv').config();
+const usernamePassword = 'bitebooker:bitebooker1';
+const bodyParser = require('body-parser');
+const express = require('express'); // Import express
+const http = require('http');
 const apiv3 = require('./apiv3methods.js');
+const port = process.env.PORT || 8080;
 
-// Load SSL certificate and key
-const options = {
-  key: fs.readFileSync('./keys/booking-server-key.pem'),
-  cert: fs.readFileSync('./keys/booking-server-cert.pem')
-};
+const app = express(); // Create an instance of express
 
-const server = https.createServer(options, (request, response) => {
+app.use(bodyParser.json());
+
+// TO-DO: implement SSL server using https module
+// for more info: https://nodejs.org/api/https.html
+// const https = require('https');
+// const fs = require('fs');
+// const options = {
+//  key: fs.readFileSync('./keys/booking-server-key.pem'),
+//  cert: fs.readFileSync('./keys/booking-server-cert.pem')
+// };
+// const server = https.createServer(options, (request, response) => {...
+
+const server = http.createServer(app);
+
+app.use((request, response, next) => {
   const {headers, method, url} = request;
 
   // Parsing basic authentication to extract base64 encoded username:password
-  var decodedString = '';
+  // Authorization:Basic dXNlcm5hbWU6cGFzc3dvcmQ=
+  let decodedString = '';
   const authorization = headers['authorization'];
   if (authorization) {
     const encodedString = authorization.replace('Basic ', '');
@@ -25,167 +36,173 @@ const server = https.createServer(options, (request, response) => {
   }
 
   if (decodedString !== usernamePassword) {
-    response.statusCode = 401;  // Unauthorized
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Unauthorized Request');
+    response.status(401).set('Content-Type', 'text/plain').end('Unauthorized Request');
     return;
   }
 
   // convert url to lower case and remove trailing '/' if there's one
-  var path = url.endsWith('/') ? url.slice(0, -1).toLowerCase() : url.toLowerCase();
+  // you can also remove prefixed URL, if your server is hosted on
+  // server/somepath/
+  let path = url.endsWith('/') ? url.slice(0, -1).toLowerCase() : url.toLowerCase();
 
   console.log(`HTTP Request ${method} ${path}`);
 
-  // retrieving request body and processing it
-  let requestBody = [];
+  request.body = [];
   request
-      .on('error', (err) => {
-        console.error(err);
-      })
-      .on('data', (chunk) => {
-        requestBody.push(chunk);
-      })
-      .on('end', () => {
-        requestBody = Buffer.concat(requestBody).toString();
-
-        var httpCode = 200;  // OK
-        var responseBody = '';
-        var contentType = 'application/json';
-
-        if (method === 'GET') {
-          // GET /v3/HealthCheck/
-          if (path === '/v3/healthcheck') {
-            try {
-              responseBody = apiv3.HealthCheck(requestBody);
-            } catch (e) {
-              httpCode = 500;  // Internal Server Error
-              console.log(`Error: ${e}`);
-            }
-          } else {
-            httpCode = 400;  // Bad Request
-            contentType = 'text/plain';
-            responseBody = 'Request Not Supported';
-          }
-        } else if (method === 'POST') {
-          switch (path) {
-            case '/v3/batchavailabilitylookup':
-              try {
-                responseBody = apiv3.BatchAvailabilityLookup(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/checkavailability':
-              try {
-                responseBody = apiv3.CheckAvailability(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/createbooking':
-              try {
-                responseBody = apiv3.CreateBooking(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/updatebooking':
-              try {
-                responseBody = apiv3.UpdateBooking(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/getbookingstatus':
-              try {
-                responseBody = apiv3.GetBookingStatus(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/listbookings':
-              try {
-                responseBody = apiv3.ListBookings(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/checkorderfulfillability':
-              try {
-                responseBody = apiv3.CheckOrderFulfillability(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/createorder':
-              try {
-                responseBody = apiv3.CreateOrder(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/listorders':
-              try {
-                responseBody = apiv3.ListOrders(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/batchgetwaitestimates':
-              try {
-                responseBody = apiv3.BatchGetWaitEstimates(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/createwaitlistentry':
-              try {
-                responseBody = apiv3.CreateWaitlistEntry(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/getwaitlistentry':
-              try {
-                responseBody = apiv3.GetWaitlistEntry(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            case '/v3/deletewaitlistentry':
-              try {
-                responseBody = apiv3.DeleteWaitlistEntry(requestBody);
-              } catch (e) {
-                httpCode = 500;  // Internal Server Error
-                console.log(`Error: ${e}`);
-              }
-              break;
-            default:
-              httpCode = 400;  // Bad Request
-              contentType = 'text/plain';
-              responseBody = 'Request Not Supported';
-          }
-        }
-
-        console.log(`HTTP Response ${httpCode} ${responseBody}`);
-        response.statusCode = httpCode;
-        response.setHeader('Content-Type', contentType);
-        response.end(responseBody);
-      });
+    .on('error', (err) => console.error(err))
+    .on('data', (chunk) => request.body.push(chunk))
+    .on('end', () => {
+      request.body = Buffer.concat(request.body).toString();
+      next();
+    });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Booking Server is running at https://${hostname}:${port}`);
+app.get('/v3/healthcheck', (request, response) => {
+  try {
+    const responseBody = apiv3.HealthCheck(request.body);
+    console.log(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/batchavailabilitylookup', (request, response) => {
+  try {
+    console.log(`Received request for /v3/batchavailabilitylookup: ${request.body}`);
+    const responseBody = apiv3.BatchAvailabilityLookup(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/checkavailability', (request, response) => {
+  try {
+    const responseBody = apiv3.CheckAvailability(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/createbooking', (request, response) => {
+  try {
+    const responseBody = apiv3.CreateBooking(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/updatebooking', (request, response) => {
+  try {
+    const responseBody = apiv3.UpdateBooking(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/getbookingstatus', (request, response) => {
+  try {
+    const responseBody = apiv3.GetBookingStatus(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/listbookings', (request, response) => {
+  try {
+    const responseBody = apiv3.ListBookings(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/checkorderfulfillability', (request, response) => {
+  try {
+    const responseBody = apiv3.CheckOrderFulfillability(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/createorder', (request, response) => {
+  try {
+    const responseBody = apiv3.CreateOrder(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/listorders', (request, response) => {
+  try {
+    const responseBody = apiv3.ListOrders(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/batchgetwaitestimates', (request, response) => {
+  try {
+    const responseBody = apiv3.BatchGetWaitEstimates(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/createwaitlistentry', (request, response) => {
+  try {
+    const responseBody = apiv3.CreateWaitlistEntry(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/getwaitlistentry', (request, response) => {
+  try {
+    const responseBody = apiv3.GetWaitlistEntry(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.post('/v3/deletewaitlistentry', (request, response) => {
+  try {
+    const responseBody = apiv3.DeleteWaitlistEntry(request.body);
+    response.status(200).json(responseBody);
+  } catch (e) {
+    response.status(500).end(`Error: ${e}`);
+    console.log(`Error: ${e}`);
+  }
+});
+
+app.use((request, response) => {
+  response.status(400).set('Content-Type', 'text/plain').end('Request Not Supported');
+});
+
+server.listen(port, () => {
+  console.log(`Booking Server is running at ${port}`);
 });
